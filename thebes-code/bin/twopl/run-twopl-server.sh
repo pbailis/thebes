067067e5970f3f58bf015d743d722b2765617b7e
ilikerps <ilikerps@gmail.com>
2013-02-01 12:35:02 -0800

    ea

--- thebes-code/bin/twopl/run-twopl-server.sh
+++ thebes-code/bin/twopl/run-twopl-server.sh
@@ -1,1 +1,1 @@
-java  -Dclusterid 1 -Dserverid 0 -Dstandalone -jar twopl/server/target/twopl-server-1.0-SNAPSHOT.jar
+java  -ea -Dclusterid 1 -Dserverid 0 -Dstandalone -jar twopl/server/target/twopl-server-1.0-SNAPSHOT.jar
