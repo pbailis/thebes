6cfc2e942576b6249d71af432420fd8acd8fab77
Peter Bailis <pbailis@cs.berkeley.edu>
2013-02-19 23:06:00 -0800

    Broken build; still needs minor Exception work; checkpointing

---
+++ ycsb-0.1.4/thebes/src/main/java/com/yahoo/ycsb/db/ThebesYCSBClient.java
@@ -0,0 +1,87 @@
+package com.yahoo.ycsb.db;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Vector;
+
+import com.yahoo.ycsb.ByteArrayByteIterator;
+import org.apache.log4j.Logger;
+
+import edu.berkeley.thebes.client.ThebesClient;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransportException;
+
+import com.yahoo.ycsb.DB;
+import com.yahoo.ycsb.DBException;
+import com.yahoo.ycsb.ByteIterator;
+
+
+public class ThebesYCSBClient extends DB {
+
+    ThebesClient client;
+
+    private final Logger logger = Logger.getLogger(ThebesYCSBClient.class);
+    
+    public static final int OK = 0;
+    public static final int ERROR = -1;
+    public static final int NOT_FOUND = -2;
+    
+	public void init() throws DBException {
+        client = new ThebesClient();
+        try {
+            client.open();
+        } catch (Exception e) {
+            throw new DBException(e.getMessage());
+        }
+	}
+	
+	public void cleanup() throws DBException {
+        client.close();
+	}
+
+    public void beginTransaction() {
+        client.beginTransaction();
+    }
+
+    public void endTransaction() {
+        client.endTransaction();
+    }
+	
+	@Override
+	public int delete(String table, String key) {
+        client.put(key, null);
+        return OK;
+	}
+
+	@Override
+	public int insert(String table, String key, HashMap<String, ByteIterator> values) {
+        client.put(key, ByteBuffer.wrap(values.values().iterator().next().toArray()));
+		return OK;
+	}
+
+	@Override
+	public int read(String table, String key, Set<String> fields,
+			HashMap<String, ByteIterator> result) {
+        result.put(fields.iterator().next(), new ByteArrayByteIterator(client.get(key).array()));
+		return OK;
+	}
+
+	@Override
+	public int scan(String table, String startkey, int recordcount,
+			Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+        logger.warn("Thebes scans are not implemented!");
+		return ERROR;
+	}
+
+	@Override
+	public int update(String table, String key, HashMap<String, ByteIterator> values) {
+        //update doesn't pass in the entire record, so we'd need to do read-modify-write
+        throw new UnsupportedOperationException("Thebes updates are not implemented!");
+	}
+	
+	private int checkStore(String table) {
+		return OK;
+	}
+}
