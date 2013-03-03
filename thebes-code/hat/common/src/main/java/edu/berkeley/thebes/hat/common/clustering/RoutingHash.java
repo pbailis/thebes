1e82778effe58b2bd700bca2081dc18a162e6636
Aaron Davidson <ilikerps@gmail.com>
2013-03-03 15:46:10 -0800

    Absolute value of routing hash fix

--- thebes-code/hat/common/src/main/java/edu/berkeley/thebes/hat/common/clustering/RoutingHash.java
+++ thebes-code/hat/common/src/main/java/edu/berkeley/thebes/hat/common/clustering/RoutingHash.java
@@ -2,6 +2,6 @@ package edu.berkeley.thebes.hat.common.clustering;
 
 public class RoutingHash {
     public static int hashKey(String key, int numServers) {
-        return key.hashCode() % numServers;
+        return Math.abs(key.hashCode()) % numServers;
     }
 }
