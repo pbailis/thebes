f37ee1c22af4f108e28e56f0f1c06ec267acfbfe
Aaron Davidson <ilikerps@yahoo.com>
2013-03-19 10:13:07 -0700

    Back to client atomicity.

--- thebes-code/scripts/setup_hosts.py
+++ thebes-code/scripts/setup_hosts.py
@@ -784,7 +784,7 @@ if __name__ == "__main__":
                        threads=80,
                        distributionparameter=2,
                        isolation_level="READ_COMMITTED",
-                       atomicity_level="NO_ATOMICITY",
+                       atomicity_level="CLIENT",
                        recordcount=100000,
                        time=60,
                        timeout=120*10000,
