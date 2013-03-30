44349a55dcc6d968bc791b04820ac65657be75c1
Aaron Davidson <ilikerps@yahoo.com>
2013-03-29 23:26:31 -0700

    Debuggery

--- thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/QuorumReplicaRouter.java
+++ thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/QuorumReplicaRouter.java
@@ -144,8 +144,11 @@ public class QuorumReplicaRouter extends ReplicaRouter {
         public void process(ReplicaClient replica) {
             PutCallback callback = new PutCallback(replica);
             try {
+                logger.error("Calling put...");
                 replica.client.put(key, value, callback);
+                logger.error("Called put...");
             } catch (TException e) {
+                logger.error("Exception happened!");
                 callback.onError(e);
             }
         }
