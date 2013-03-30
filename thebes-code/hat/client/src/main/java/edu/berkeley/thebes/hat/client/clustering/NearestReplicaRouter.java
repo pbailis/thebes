6058458d73a2d7357e9d422d9a04dfa4dc13753c
Peter Bailis <pbailis@cs.berkeley.edu>
2013-03-30 04:35:47 -0700

    Requiredversion null hack

--- thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/NearestReplicaRouter.java
+++ thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/NearestReplicaRouter.java
@@ -136,11 +136,11 @@ public class NearestReplicaRouter extends ReplicaRouter {
                 toFetch.put(client, new HashMap<String, ThriftVersion>());
             }
 
-            Version requiredVersion = keys.get(key);
-            if(requiredVersion == null)
-                requiredVersion = Version.NULL_VERSION;
+            ThriftVersion requiredVersion = null;
+            if(keys.get(key) != null)
+                requiredVersion = keys.get(key).getThriftVersion();
 
-            toFetch.get(client).put(key, requiredVersion.getThriftVersion());
+            toFetch.get(client).put(key, requiredVersion);
         }
 
         Semaphore notifyDone = new Semaphore(0);
