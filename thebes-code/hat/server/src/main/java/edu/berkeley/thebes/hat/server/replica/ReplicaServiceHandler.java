24fa8cd4ddc7083a6cde4c06ed47edecf5892569
Aaron Davidson <ilikerps@yahoo.com>
2013-04-07 17:14:56 -0700

    Oopsie

--- thebes-code/hat/server/src/main/java/edu/berkeley/thebes/hat/server/replica/ReplicaServiceHandler.java
+++ thebes-code/hat/server/src/main/java/edu/berkeley/thebes/hat/server/replica/ReplicaServiceHandler.java
@@ -94,7 +94,7 @@ public class ReplicaServiceHandler implements ReplicaService.Iface {
                              "' requiredVersion: "+ requiredVersion+
                              ", found version: " + (ret == null ? null : ret.getVersion()));
     
-            if ((ret == null || requiredVersion.compareTo(ret.getVersion()) > 0))
+            if (ret == null || ret.getVersion().compareTo(Version.NULL_VERSION) == 0)
                 nullVersionsMeter.mark();
             if (requiredVersion != null && requiredVersion.compareTo(Version.NULL_VERSION) != 0 &&
                     (ret == null || requiredVersion.compareTo(ret.getVersion()) > 0)) {
