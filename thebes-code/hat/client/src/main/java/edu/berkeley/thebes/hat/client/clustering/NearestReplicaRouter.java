540ed21ec17a64248b35ad05ac3813f3b70bf844
Aaron Davidson <ilikerps@yahoo.com>
2013-04-12 22:53:25 -0700

    Let's find slow nodes!

--- thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/NearestReplicaRouter.java
+++ thebes-code/hat/client/src/main/java/edu/berkeley/thebes/hat/client/clustering/NearestReplicaRouter.java
@@ -1,8 +1,12 @@
 package edu.berkeley.thebes.hat.client.clustering;
 
-import org.apache.log4j.Logger;
 import org.apache.thrift.TException;
 import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 
 import edu.berkeley.thebes.common.clustering.RoutingHash;
 import edu.berkeley.thebes.common.config.Config;
@@ -17,11 +21,21 @@ import edu.berkeley.thebes.hat.common.thrift.ThriftUtil;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class NearestReplicaRouter extends ReplicaRouter {
+    
+    private static Logger logger = LoggerFactory.getLogger(NearestReplicaRouter.class);
 
+    private static final double ALPHA = .95;
+    private static final double TIME_BETWEEN_CHECKS = 5000;
+    private static final double WARNING_THRESHOLD = 2;
     private List<ReplicaService.Client> syncReplicas;
-    private List<ReplicaService.AsyncClient> asyncReplicas;
+    private Map<ServerAddress, Double> averageLatencyByServer;
+    
+    private long timeSinceLastCheck;
 
     public NearestReplicaRouter() throws TTransportException, IOException {
         assert(Config.getRoutingMode() == RoutingMode.NEAREST);
@@ -29,15 +43,15 @@ public class NearestReplicaRouter extends ReplicaRouter {
         List<ServerAddress> serverIPs = Config.getServersInCluster();
         
         syncReplicas = new ArrayList<ReplicaService.Client>(serverIPs.size());
-        asyncReplicas = new ArrayList<ReplicaService.AsyncClient>(serverIPs.size());
+        averageLatencyByServer = Maps.newHashMap();
 
         for (ServerAddress server : serverIPs) {
-            Logger.getLogger(this.getClass()).debug("Connecting to " + server);
             syncReplicas.add(ThriftUtil.getReplicaServiceSyncClient(server.getIP(), server.getPort()));
-            Logger.getLogger(this.getClass()).debug("Connecting to (async) " + server);
-            asyncReplicas.add(ThriftUtil.getReplicaServiceAsyncClient(server.getIP(), server.getPort()));
-            Logger.getLogger(this.getClass()).debug("Done!");
+            averageLatencyByServer.put(server, 0d);
+            logger.trace("Connected to " + server);
         }
+        
+        timeSinceLastCheck = 0;
     }
     
     private ReplicaService.Client getSyncReplicaByKey(String key) {
@@ -51,10 +65,48 @@ public class NearestReplicaRouter extends ReplicaRouter {
     @Override
     public boolean put(String key, DataItem value) throws TException {
         try {
-            return getSyncReplicaByKey(key).put(key, value.toThrift());
+            ServerAddress serverAddress = getReplicaIPByKey(key);
+            ReplicaService.Client serverClient = getSyncReplicaByKey(key); 
+            long startTime = System.currentTimeMillis();
+            boolean ret = serverClient.put(key, value.toThrift());
+            
+            long duration = System.currentTimeMillis() - startTime;
+            averageLatencyByServer.put(serverAddress,
+                    averageLatencyByServer.get(key)*ALPHA + duration*(1-ALPHA));
+            
+            checkLatencies();
+            return ret;
         } catch (TException e) {
             throw new TException("Failed to write to " + getReplicaIPByKey(key), e);
         }
+    }
+    
+    private void checkLatencies() {
+        if (timeSinceLastCheck == 0) {
+            timeSinceLastCheck = System.currentTimeMillis();
+        }
+        
+        if (System.currentTimeMillis() - timeSinceLastCheck < TIME_BETWEEN_CHECKS) {
+            return;
+        }
+        
+        
+        double minLatency = -1;
+        for (double latency : averageLatencyByServer.values()) {
+            if (minLatency == -1 || latency < minLatency) {
+                minLatency = latency;
+            }
+        }
+        
+        for (ServerAddress server : averageLatencyByServer.keySet()) {
+            double latency = averageLatencyByServer.get(server);
+            if (latency > WARNING_THRESHOLD * minLatency) {
+                logger.warn("Server " + server + " has high avg put latency: " + latency
+                        + " (min avg latency=" + minLatency + ")");
+            }
+        }
+        
+        timeSinceLastCheck = System.currentTimeMillis();
     }
 
     @Override
