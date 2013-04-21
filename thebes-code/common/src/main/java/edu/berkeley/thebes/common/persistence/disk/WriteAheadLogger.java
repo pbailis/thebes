74699907357e70dbdc125775d42c771ffb0ca5b9
Aaron Davidson <ilikerps@yahoo.com>
2013-04-21 16:59:54 -0700

    Oopsie

--- thebes-code/common/src/main/java/edu/berkeley/thebes/common/persistence/disk/WriteAheadLogger.java
+++ thebes-code/common/src/main/java/edu/berkeley/thebes/common/persistence/disk/WriteAheadLogger.java
@@ -54,7 +54,6 @@ public class WriteAheadLogger {
         }
         
         public void writeCompleted() {
-            assert latch.isHeldByCurrentThread();
             this.writeCompleted.set(true);
         }
         
